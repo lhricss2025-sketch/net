51d4c35d488c17692e2b8e5c139c8c7e830a3c04
senzo_12 <lhricss2025@gmail.com>
2026-08-02 08:13:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN", "your-auth-token")
 ADMIN_IDS = [int(id) for id in os.getenv("ADMIN_IDS", "123456789").split(",")]
 
 FORCE_JOIN_CHANNELS = [
-    {"id": "-3774536485", "link": "https://t.me/+wRaiiDUT9DB41ZiVE9"},
+    {"id": "Premium Stuff", "link": "https://t.me/+wRaiiDUT9DB41ZiVE9"},
     {"id": "@Senzo_Official", "link": "https://t.me/Senzo_Official"},
 ]
 
